28b466a6d4cdd808e0dd3eaba84551df0cac4cc9
草小孟 <1263597652@qq.com>
2018-01-11 15:05:10 +0800

    Add files via upload

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+
+export default {
+  name: 'dva-admin',
+  prefix: '',
+  // footerText: 'dva-admin 版权所有 © 2016 由 pmg1989 提供支持',
+  footerText: '毕业设计管理系统',
+  // logoSrc: 'https://o9u2lnvze.qnssl.com/web/global/brand.png',
+  logoText: 'Admin',
+  needLogin: true,
+}
